aaba0c0107dc101e75dd50bf6de8c84a7e185e6f
Riona <c00296706@setu.ie>
2026-02-01 13:59:05 +0000

    fix

--- Features/GameEventTracker/game_event_tracker.gd
+++ Features/GameEventTracker/game_event_tracker.gd
@@ -1,5 +1,6 @@
 extends Node
 
+signal event_tag_added(game_tag : String)
 @onready var intro_played = false
 
 #---------------------------------------------------------------------------------------------------
