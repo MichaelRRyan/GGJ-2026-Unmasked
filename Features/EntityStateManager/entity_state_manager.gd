8134f35a706d118094170a97b22bf4ce8aa5c329
Michael R Ryan <mrainsfordryan@gmail.com>
2026-02-02 18:39:13 +0000

    Fixed audio issues and added web export

--- Features/EntityStateManager/entity_state_manager.gd
+++ Features/EntityStateManager/entity_state_manager.gd
@@ -9,22 +9,26 @@ var bgm_player = AudioStreamPlayer.new()
 
 func _setup_audio() -> void:
 	add_child(bgm_player)
-	print("loaded song")
 	_select_audio()
 
 
 func _select_audio():
 	var current_path = get_tree().current_scene.scene_file_path
 	if current_path == "res://Levels/level_bar.tscn":
-		bgm_player.stream = load("res://Assets/Audio/unmasked_bar_theme.wav")
+		_load_and_play_audio("res://Assets/Audio/unmasked_bar_theme.wav")
 	elif current_path == "res://Levels/level_witch_tower.tscn":
-		bgm_player.stream = load("res://Assets/Audio/unmasked_witch_theme.wav")
+		_load_and_play_audio("res://Assets/Audio/unmasked_witch_theme.wav")
 	elif current_path == "res://Levels/level_bakery.tscn": # Fixed logic here
-		bgm_player.stream = load("res://Assets/Audio/unmasked_end_screen.wav")
+		_load_and_play_audio("res://Assets/Audio/unmasked_end_screen.wav")
 	else: 
-		bgm_player.stream = load("res://Assets/Audio/unmasked_theme.mp3")
+		_load_and_play_audio("res://Assets/Audio/unmasked_theme.mp3")
 	
-	bgm_player.play(0.0)
+
+func _load_and_play_audio(path : String):
+	var new_song = load(path)
+	if bgm_player.stream != new_song:
+		bgm_player.stream = new_song
+		bgm_player.play(0.0)
 
 
 func _ready() -> void:
